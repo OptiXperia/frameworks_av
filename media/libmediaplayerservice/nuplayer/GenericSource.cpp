27e6074443ef90164339fdcdf4ab4d580771dc63
Surajit Podder <spodder@codeaurora.org>
2014-11-20 18:16:33 +0530

    NuPlayer: Disable widevine flag for L3 content
    Disable widevine flag for L3 content. This is
    needed because Generic source considers widevine
    playback to be L1 by default, without checking if
    secure buffers are requested by the widevine source.
    
    Change-Id: I4e33836dfa98554d5ae0cb8cc01c1c960ba86fb1

--- media/libmediaplayerservice/nuplayer/GenericSource.cpp
+++ media/libmediaplayerservice/nuplayer/GenericSource.cpp
@@ -246,6 +246,8 @@ status_t NuPlayer::GenericSource::initFromDataSource() {
                     if (mUIDValid) {
                         extractor->setUID(mUID);
                     }
+                } else {
+                     mIsWidevine = false;
                 }
             }
         }
